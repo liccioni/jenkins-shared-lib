83940b3b682b5b23ab9ebeec773982c6d95abef4
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2019-12-26 19:17:09 +0100

    adding test step

--- vars/deliveryPipeline.groovy
+++ vars/deliveryPipeline.groovy
@@ -16,7 +16,14 @@ def call(body) {
             }
             stage('Test') {
                 steps {
-                    echo getCommitHash()
+                    script {
+                        try {
+                            sh './gradlew clean test --no-daemon' //run a gradle task
+                        } finally {
+                            junit '**/build/test-results/test/*.xml' //make the junit test results available in any case (success & failure)
+                        }
+                    }
+
                 }
             }
             stage('Deploy') {
