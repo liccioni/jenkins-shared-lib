2fed560a65605dbefe3d36a06350a9341707b35e
Gustavo Rodriguez <gustavo.rodriguez@adevinta.com>
2020-01-03 23:44:51 +0100

    adjusting docker-registry

--- vars/deliveryPipeline.groovy
+++ vars/deliveryPipeline.groovy
@@ -50,18 +50,18 @@ def call(body) {
                     withEnv(['VERSION=' + env.version.trim(), 'COMMIT=' + env.commit.trim()]) {
                         sh """
                             docker build \
-                            -t docker-registry:5000/liccioni/first-app:${VERSION}.${COMMIT}  \
-                            -t docker-registry:5000/liccioni/first-app:latest \
+                            -t docker-registry.192.168.26.11.nip.io/liccioni/first-app:${VERSION}.${COMMIT}  \
+                            -t docker-registry.192.168.26.11.nip.io/liccioni/first-app:latest \
                             .
                            """
                     }
                 }
 
                 stage('Push image') {
-                    withDockerRegistry([credentialsId: 'docker-registry-credentials', url: 'http://docker-registry:5000']) {
+                    withDockerRegistry([credentialsId: 'docker-registry-credentials', url: 'https://docker-registry.192.168.26.11.nip.io']) {
                         withEnv(['VERSION=' + env.VERSION.trim(), 'COMMIT=' + env.COMMIT.trim()]) {
-                            sh "docker push docker-registry:5000/liccioni/first-app:${VERSION}.${COMMIT}"
-                            sh 'docker push docker-registry:5000/liccioni/first-app:latest'
+                            sh "docker push docker-registry.192.168.26.11.nip.io/liccioni/first-app:${VERSION}.${COMMIT}"
+                            sh 'docker push docker-registry.192.168.26.11.nip.io/liccioni/first-app:latest'
                         }
                     }
                 }
